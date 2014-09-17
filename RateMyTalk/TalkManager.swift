e57ee101dd316656995195058e81ce54b3522588
StuFF mc <mc@stuffmc.com>
2014-09-17 17:47:26 +0200

    Importing of the sessions works! They are all on CloudKit

--- RateMyTalk/TalkManager.swift
+++ RateMyTalk/TalkManager.swift
@@ -16,33 +16,35 @@ class TalkManager {
     let allQuery = CKQuery(recordType: "Talks", predicate: NSPredicate(value: true))
     
     init() {
-        deleteAllTalks()
-        // CKAsset for Speaker Photo!
-        
     }
     
     func importTalks() {
         let url = NSBundle.mainBundle().URLForResource("Schedule2014", withExtension: "csv")
         let string = NSString(contentsOfURL: url!, encoding: NSASCIIStringEncoding, error: nil)
         let sessions = string.componentsSeparatedByString("\n")
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.timeStyle = .ShortStyle
+        dateFormatter.dateStyle = .ShortStyle
+        dateFormatter.locale = NSLocale(localeIdentifier: "de_DE")
+
         for session in sessions {
             let attributes = session.componentsSeparatedByString(";")
-            //            println(attributes)
             let talkRecord = CKRecord(recordType: talks)
-            //            talkRecord.setValue(attributes[0], forKey: "name")
-            talkRecord.setValue(attributes[1], forKey: "begin")
-            talkRecord.setValue(attributes[2], forKey: "end")
+            let begin = dateFormatter.dateFromString("\(attributes[0]) \(attributes[1])")
+            let end = dateFormatter.dateFromString("\(attributes[0]) \(attributes[2])")
+            
+            talkRecord.setValue(begin, forKey: "begin")
+            talkRecord.setValue(end, forKey: "end")
             
             talkRecord.setValue(attributes[3], forKey: "name")
             if attributes.count > 4 {
                 talkRecord.setValue(attributes[4], forKey: "speaker")
             }
             
-            sleep(3)
+            sleep(2)
             publicDB.saveRecord(talkRecord, completionHandler: { (savedTalk, error) -> Void in
                 println(savedTalk)
                 println(error)
-                //                self.fetchAllTalks()
             })
         }
     }
