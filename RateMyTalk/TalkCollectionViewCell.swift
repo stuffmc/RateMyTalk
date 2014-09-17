3b2baeb027488ec053c03935dfadad01c95f9d2a
Mircea Vasiliniuc <mvasiliniuc@gmail.com>
2014-09-17 19:07:15 +0300

    Add dummy collection view

---
+++ RateMyTalk/TalkCollectionViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  TalkCollectionViewCell.swift
+//  RateMyTalk
+//
+//  Created by Mircea on 17/09/14.
+//  Copyright (c) 2014 Manuel "StuFF mc" Carrasco Molina. All rights reserved.
+//
+
+import UIKit
+
+class TalkCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var lblTime: UILabel!
+    @IBOutlet weak var lblTopic: UILabel!
+    @IBOutlet weak var lblSpeaker: UILabel!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
