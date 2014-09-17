f0a0f2ad31565d1392171e940f80e4920afdff62
StuFF mc <mc@stuffmc.com>
2014-09-17 11:41:39 +0200

    Basic Scaffold of the app and 3 Teams

---
+++ RateMyTalk/RateTalkVC.swift
@@ -0,0 +1,18 @@
+//
+//  RateTalkVC.swift
+//  RateMyTalk
+//
+//  Created by Manuel "StuFF mc" Carrasco Molina on 17/09/14.
+//  Copyright (c) 2014 Manuel "StuFF mc" Carrasco Molina. All rights reserved.
+//
+
+import UIKit
+
+class RateTalkVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
---
+++ RateMyTalk/TalkManager.swift
@@ -0,0 +1,17 @@
+//
+//  TalkManager.swift
+//  RateMyTalk
+//
+//  Created by Manuel "StuFF mc" Carrasco Molina on 17/09/14.
+//  Copyright (c) 2014 Manuel "StuFF mc" Carrasco Molina. All rights reserved.
+//
+
+import Foundation
+
+class TalkManager {
+    
+    init() {
+        
+    }
+    
+}
