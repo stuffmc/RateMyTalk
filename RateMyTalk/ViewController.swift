9929ecb5ea4444a56cca3a9e8aaacb9063ea05c4
StuFF mc <mc@stuffmc.com>
2014-09-17 11:31:23 +0200

    Initial Commit

---
+++ RateMyTalk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RateMyTalk
+//
+//  Created by Manuel "StuFF mc" Carrasco Molina on 17/09/14.
+//  Copyright (c) 2014 Manuel "StuFF mc" Carrasco Molina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
