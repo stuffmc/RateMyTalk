dcf984148e58cbf0c60390bde9669a1a2023fb96
StuFF mc <mc@stuffmc.com>
2014-09-17 17:27:09 +0200

    Importing Data from CSV (which originally came from Excel) to Public DB... Oh boy...

--- RateMyTalk/TalkManager.swift
+++ RateMyTalk/TalkManager.swift
@@ -12,28 +12,55 @@ import CloudKit
 class TalkManager {
     let talks = "Talks"
     let publicDB = CKContainer.defaultContainer().publicCloudDatabase
-    let privateDB = CKContainer.defaultContainer().privateCloudDatabase
+//    let privateDB = CKContainer.defaultContainer().privateCloudDatabase
+    let allQuery = CKQuery(recordType: "Talks", predicate: NSPredicate(value: true))
     
     init() {
-        let talkRecord = CKRecord(recordType: talks)
-//        talkRecord["name"] = "Talk Name" // 'CKRecord' does not have a member named 'subscript'
-        talkRecord.setValue("Talk name", forKey: "name")
+        deleteAllTalks()
         // CKAsset for Speaker Photo!
-        publicDB.saveRecord(talkRecord, completionHandler: { (savedTalk, error) -> Void in
-            println(savedTalk)
-            println(error)
-            self.fetchAllTalks()
-        })
         
     }
     
-    func fetchAllTalks() {
-        let predicate = NSPredicate(value: true)
-        let query = CKQuery(recordType: talks, predicate: predicate)
-        publicDB.performQuery(query, inZoneWithID: nil) { (records, error) -> Void in
-            println(records)
-            let reference = CKReference(record: records?.first as CKRecord, action: .None)
+    func importTalks() {
+        let url = NSBundle.mainBundle().URLForResource("Schedule2014", withExtension: "csv")
+        let string = NSString(contentsOfURL: url!, encoding: NSASCIIStringEncoding, error: nil)
+        let sessions = string.componentsSeparatedByString("\n")
+        for session in sessions {
+            let attributes = session.componentsSeparatedByString(";")
+            //            println(attributes)
+            let talkRecord = CKRecord(recordType: talks)
+            //            talkRecord.setValue(attributes[0], forKey: "name")
+            talkRecord.setValue(attributes[1], forKey: "begin")
+            talkRecord.setValue(attributes[2], forKey: "end")
+            
+            talkRecord.setValue(attributes[3], forKey: "name")
+            if attributes.count > 4 {
+                talkRecord.setValue(attributes[4], forKey: "speaker")
+            }
+            
+            sleep(3)
+            publicDB.saveRecord(talkRecord, completionHandler: { (savedTalk, error) -> Void in
+                println(savedTalk)
+                println(error)
+                //                self.fetchAllTalks()
+            })
         }
     }
     
+    func fetchAllTalks() {
+        publicDB.performQuery(allQuery, inZoneWithID: nil) { (records, error) -> Void in
+            println(records)
+            let reference = CKReference(record: records.first as CKRecord, action: .None)
+        }
+    }
+
+    func deleteAllTalks() {
+        publicDB.performQuery(allQuery, inZoneWithID: nil) { (records, error) -> Void in
+            println(records)
+            for record in records {
+                self.publicDB.deleteRecordWithID((record as CKRecord).recordID, completionHandler: nil)
+            }
+        }
+    }
+
 }
